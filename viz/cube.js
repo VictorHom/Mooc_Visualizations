731f490fe3638814b913962598fca56dab489ac4
Victor Hom <vh62@cornell.edu>
2016-03-12 15:15:00 -0500

    added in tutorials from udacity graphics course

---
+++ viz/cube.js
@@ -0,0 +1,93 @@
+////////////////////////////////////////////////////////////////////////////////
+// Fixing Incorrect JavaScript exercise
+////////////////////////////////////////////////////////////////////////////////
+// Your task is to find the syntax errors in this Javacript
+// until it shows the the Gold Cube!
+// WebGL is not supported in Internet Explorer
+// There are 3 syntax errors in this code
+////////////////////////////////////////////////////////////////////////////////
+/*global THREE, Coordinates, $, document, window*/
+'use strict';
+// var THREE = require('../node_modules/three/three.js');
+var Coordinates = require('./Coordinates.js');
+require('./OrbitAndPanControls.new.js');
+var camera, scene, renderer;
+var windowScale;
+var cameraControls;
+var clock = new THREE.Clock();
+
+function drawGoldCube() {
+
+	var cube;
+	var cubeSizeLength = 100;
+	var goldColor = "#FFDF00";
+	var showFrame = true;
+	var wireMaterial = new THREE.MeshBasicMaterial( { color: goldColor, wireframe: showFrame } ) ;
+
+	var cubeGeometry = new THREE.CubeGeometry(cubeSizeLength, cubeSizeLength, cubeSizeLength);
+
+	cube = new THREE.Mesh( cubeGeometry, wireMaterial );
+	cube.position.x = 0;	// centered at origin
+	cube.position.y = 0;	// centered at origin
+	cube.position.z = 0;	// centered at origin
+	scene.add(cube) ;
+
+}
+
+function init() {
+	console.log("DO I EXISTS");
+	console.log(THREE);
+	var canvasWidth = 846;
+	var canvasHeight = 494;
+	// For grading the window is fixed in size; here's general code:
+	//var canvasWidth = window.innerWidth;
+	//var canvasHeight = window.innerHeight;
+	var canvasRatio = canvasWidth / canvasHeight;
+	// SCENE
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog( 0x808080, 2000, 4000 );
+	// LIGHTS
+	scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+	// RENDERER
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.gammaInput = true;
+	renderer.gammaOutput = true;
+	renderer.setSize(canvasWidth, canvasHeight);
+	renderer.setClearColor( scene.fog.color, 1 );
+
+	var container = document.getElementById('cube');
+	container.appendChild( renderer.domElement );
+
+
+	// CAMERA
+	camera = new THREE.PerspectiveCamera( 45, canvasRatio, 1, 4000 );
+	camera.position.set( -200, 200, -150 );
+	// CONTROLS
+	cameraControls = new THREE.OrbitAndPanControls(camera, renderer.domElement);
+	cameraControls.target.set(0,0,0);
+
+	//draw the coordinate grid
+	Coordinates.drawGrid({size:1000,scale:0.01});
+	Coordinates.drawGrid({size:1000,scale:0.01, orientation:"y"});
+	Coordinates.drawGrid({size:1000,scale:0.01, orientation:"z"});
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	render();
+}
+
+function render() {
+	var delta = clock.getDelta();
+	cameraControls.update(delta);
+	renderer.render(scene, camera);
+}
+module.exports = {
+	drawCube : function() {
+
+		init();
+		drawGoldCube();
+		animate();
+	}
+}
--- viz/main.js
+++ viz/main.js
@@ -2,11 +2,21 @@
 import './styles.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
+import { drawCube } from './cube.js';
+// var Coordinates = require('../public/public/Coordinates.js');
+// require('../public/public/OrbitAndPanControls.new.js');
+// import * from 'three';
 
 var Test = React.createClass({
 	render() {
-		return (<div>Hello Test</div>);
+		return (
+			<div>
+			<div>Hello Test</div>
+			<div id="cube"></div>
+			</div>
+		);
 	}
 });
 // add in react router;
 ReactDom.render(<Test/>, document.getElementById("mooc_visualizations"));
+drawCube();
